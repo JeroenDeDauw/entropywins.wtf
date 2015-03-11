3ebc4269cf0b6d07732254d21a314c4971a77e14
jeroendedauw <jeroendedauw@gmail.com>
2015-03-11 04:47:14 +0100

    Crimes against sanity and embedded latest blog posts via RSS

--- src/controllers.php
+++ src/controllers.php
@@ -34,6 +34,38 @@ $app->get('/wikidata', function () use ($app) {
 	return $app['twig']->render('wikidata.md', ['page' => 'wikidata']);
 });
 
+$app->get('/blog-embedded', function () use ($app) {
+	// The derp is strong in this one
+	// Should learn how to use twig properly :)
+	$html = '';
+
+	$rssReader = new SimplePie();
+	$rssReader->set_feed_url( 'http://www.bn2vs.com/blog/feed/' );
+	$rssReader->init();
+	$rssReader->handle_content_type();
+
+	/**
+	 * @var SimplePie_Item $item
+	 */
+	foreach ( $rssReader->get_items() as $item ) {
+		$pl = $item->get_permalink();
+		$title = $item->get_title();
+		$content = $item->get_content();
+		$date = $item->get_date('j F Y | H:i');
+
+		$html .= <<<EOL
+		<div class="item">
+			<h2><a href="$pl">$title</a></h2>
+			<p><small>Posted on $date</small></p>
+			$content
+		</div>
+EOL;
+
+	}
+
+	return $app['twig']->render('blog.html', ['page' => 'blog-embedded', 'blogposts' => $html]);
+});
+
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
         return;
