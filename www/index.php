eee6eb006e238a5fac37b4c7c6000cb3398ea3ee
jeroendedauw <jeroendedauw@gmail.com>
2015-10-15 17:54:28 +0200

    Fix old blog links
    Perhaps not the proper approach, though I don't want to waste
    more of my life configuring apache >_>

--- www/index.php
+++ www/index.php
@@ -2,6 +2,12 @@
 
 ini_set('display_errors', 0);
 
+$hostParts = explode( '.', $_SERVER['HTTP_HOST'] );
+if ( array_shift( $hostParts ) === 'blog' ) {
+	header( 'Location: http://www.bn2vs.com/blog' . $_SERVER['REQUEST_URI'] );
+	die();
+}
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../src/app.php';
