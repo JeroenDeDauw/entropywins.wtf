8ccb7f92facdefe4542f06ff737401a8a44a6556
jeroendedauw <jeroendedauw@gmail.com>
2015-12-22 21:41:02 +0100

    Add smoke test

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -16,6 +16,31 @@ class SmokeTest extends WebTestCase {
 		$this->assertSame( 200, $client->getResponse()->getStatusCode() );
 	}
 
+	public function testRootHasUnderConstruction() {
+		$client = $this->createClient();
 
+		$client->request( 'GET', '/' );
+
+		$this->assertContains( 'Under construction', $client->getResponse()->getContent() );
+	}
+
+	public function testSmwPageHasSemanticMediaWiki() {
+		$client = $this->createClient();
+
+		$client->request( 'GET', '/smw' );
+
+		$this->assertContains( 'Semantic MediaWiki', $client->getResponse()->getContent() );
+	}
+
+	public function testSmwPageHasRenderedMarkdown() {
+		$client = $this->createClient();
+
+		$client->request( 'GET', '/smw' );
+
+		$this->assertContains(
+			'<a href="https://www.ohloh.net/p/smw/contributors">contributors list on Ohloh</a>',
+			$client->getResponse()->getContent()
+		);
+	}
 
 }
