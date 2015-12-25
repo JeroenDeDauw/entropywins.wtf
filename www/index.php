a96352ccd4424beb13aa96f5c12b9660b3cd8ee4
jeroendedauw <jeroendedauw@gmail.com>
2015-12-26 00:03:03 +0100

    Update blog url

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 0);
 
 $hostParts = explode( '.', $_SERVER['HTTP_HOST'] );
 if ( array_shift( $hostParts ) === 'blog' ) {
-	header( 'Location: http://www.bn2vs.com/blog' . $_SERVER['REQUEST_URI'] );
+	header( 'Location: http://www.entropywins.wtf/blog' . $_SERVER['REQUEST_URI'] );
 	die();
 }
 
