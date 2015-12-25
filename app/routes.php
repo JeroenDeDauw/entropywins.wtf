9f316b52c34c471def04c8df52d7c31deac12e4f
jeroendedauw <jeroendedauw@gmail.com>
2015-12-25 23:29:05 +0100

    Add latest blog posts list to home page

--- app/routes.php
+++ app/routes.php
@@ -4,10 +4,7 @@
  * @var Silex\Application $app
  */
 
-$app->get('/', function () use ($app) {
-	// TODO: learn how to access page name more sanely if possible
-	return $app['twig']->render('index.html', ['page' => 'home']);
-});
+// TODO: learn how to access page name more sanely if possible
 
 $app->get('/home', function () use ($app) {
 	return $app['twig']->render('index.html', ['page' => 'home']);
@@ -65,3 +62,32 @@ EOL;
 	return $app['twig']->render('blog.html', ['page' => 'blog-embedded', 'blogposts' => $html]);
 });
 
+
+$app->get('/', function () use ($app) {
+	// The derp is strong in this one
+	// TODO: Should learn how to use twig properly :)
+	$html = '';
+
+	$rssReader = new SimplePie();
+	$rssReader->set_feed_url( 'http://www.bn2vs.com/blog/feed/' );
+	$rssReader->init();
+	$rssReader->handle_content_type();
+
+	/**
+	 * @var SimplePie_Item $item
+	 */
+	foreach ( $rssReader->get_items() as $item ) {
+		$pl = $item->get_permalink();
+		$title = $item->get_title();
+		$date = $item->get_date('j F Y | H:i');
+
+		$html .= <<<EOL
+		<li><a href="$pl">$title</a> <small>($date)</small></li>
+EOL;
+
+	}
+
+	$html = '<ul>' . $html . '</ul>';
+
+	return $app['twig']->render('index.html', ['page' => 'home', 'blogposts' => $html]);
+});
